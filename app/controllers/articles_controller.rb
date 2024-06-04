9cb04831c6a6d87eacf4efb12e7d72c06843a270
Jaxon Avena <jaxonavena@gmail.com>
2024-06-03 22:35:10 -0500

    Initial commit to create the repo. I've already created an article model and controller that's completely outfitted with CRUD routes. The respective views are also implemented already. The db contains an article table. There is one problem though -- the destroy method isn't functioning properly yet for articles. It's trying to use Turbo which is built into Rails 7, but I'm on Rails 6 because that's what my machine is configured to for work. According to the Turbo github page I can manually install it with the gemfile and whatnot, but I've tried to do so, and I still can't get it working. I will obviously have to figure that out.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # instantiates new article but doesn't save it -- this article will be used in the view when building the form
+  def new
+    @article = Article.new
+  end
+
+  # instantiates new article and attempts to save it
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      # Else, the action redisplays the form by rendering app/views/articles/new.html.erb with status code 422 Unprocessable Entity.
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
