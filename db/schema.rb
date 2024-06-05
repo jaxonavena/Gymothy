4d944cabe6f0f568887b7c9a03fc9e453c4b313b
Jaxon Avena <jaxonavena@gmail.com>
2024-06-05 09:25:59 -0500

    Upgrade Rails to 7.1.3.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2024_05_30_025450) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_141953) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
