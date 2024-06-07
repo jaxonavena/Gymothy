0f023f7bf21f71fd9cc7c55a4e47ca5f46bd6639
Jaxon Avena <jaxonavena@gmail.com>
2024-06-06 22:33:55 -0500

    Fixes: #5
    This is just section 9.3 of the Rails Getting Started guide. I added a concern for the visibility of the 'article.rb' and 'comment.rb' models. Visiblity meaning public, private, or archived. Then I refactored some of the code to work with the new concern.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,17 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  # All of the below status stuff is now coming from the "include Visible" at the top of the model
+  # VALID_STATUSES = ['public', 'private', 'archived']
+
+  # validates :status, inclusion: { in: VALID_STATUSES }
+
+  # def archived?
+  #   status == 'archived'
+  # end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,14 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
+
+  # All of the below status stuff is now coming from the "include Visible" at the top of the model
+  # VALID_STATUSES = ['public', 'private', 'archived']
+
+  # validates :status, inclusion: { in: VALID_STATUSES }
+
+  # def archived?
+  #   status == 'archived'
+  # end
 end
