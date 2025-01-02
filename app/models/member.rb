842f973dff887f9b98cf65a790281264621c8383
Jaxon Avena <jaxonavena@gmail.com>
2025-01-02 13:00:22 -0600

    Horribly ugly, but I'm coming back in Jan since August, so this is the current state of this branch, and I'm gonna go check out how main is doing.

--- app/models/member.rb
+++ app/models/member.rb
@@ -13,6 +13,11 @@
 #  updated_at   :datetime         not null
 #
 class Member < ApplicationRecord
+  has_many :memberships, dependent: destroy
+  has_many :businesses, through: :memberships
+  # has_many :visits
+  # has_many :sites, through: :visits
+
   validates :name, presence: true
   validates :email, presence: true, unless: -> { phone_number.present? }
   validates :phone_number, presence: true, unless: -> { email.present? }
---
+++ spec/factories/business.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :business do
+    name { "Big Business" }
+    owner_name { "Jaxon" }
+    owner_phone { "000-111-1234" }
+  end
+end
