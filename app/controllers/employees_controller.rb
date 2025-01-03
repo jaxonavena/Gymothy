c38eb7b989991980bd25d13b5e9793c06907bdf4
Jaxon Avena <jaxonavena@gmail.com>
2025-01-03 12:16:21 -0600

    Add employee #create form
    Now, from the Business #show page, you can click 'add employee' and fill out a form to create a new associated employee.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,5 @@
 class EmployeesController < ApplicationController
+  before_action :set_business
   before_action :set_employee, only: %i[show edit update destroy]
 
   def index
@@ -9,14 +10,20 @@ class EmployeesController < ApplicationController
   end
 
   def new
-    @employee = Employee.new
+    @employee = @business.employees.new
   end
 
   def edit
   end
 
   def create
-    @employee = Employee.new(employee_params)
+    @employee = @business.employees.new(employee_params)
+
+    if @employee.save
+      redirect_to business_employee_path(@business, @employee)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -27,6 +34,10 @@ class EmployeesController < ApplicationController
   end
 
   private
+
+  def set_business
+    @business = Business.find(params[:business_id])
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_employee
