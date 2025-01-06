9a933f6d6b1b1e8b604b038bbd2a592302c4ef8d
Jaxon Avena <jaxonavena@gmail.com>
2025-01-06 10:59:32 -0600

    Get config form working
    Now when you submit your changes on the edit page, the newly set configurations will actually save to the model.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -30,10 +30,12 @@ class BusinessesController < ApplicationController
   end
 
   def update
-    if @business.save
-      redirect_to @business
+    if @business.update(business_params)
+      @business.toggle_schedule_display if @business.saved_change_to_display_schedule?
+
+      redirect_to @business, notice: "Business updated successfully!"
     else
-      redirect_to edit_business_path(@business)
+      redirect_to edit_business_path(@business), alert: "Unable to update business."
     end
   end
 
@@ -44,6 +46,6 @@ class BusinessesController < ApplicationController
   end
 
   def business_params
-    params.require(:business).permit(:name)
+    params.require(:business).permit(:name, :display_schedule)
   end
 end
