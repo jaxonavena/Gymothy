dd165e941a73e124eea35f5434aba3fe867f06f3
Jaxon Avena <jaxonavena@gmail.com>
2025-01-13 13:05:37 -0600

    Reroute all the default paths
    Because the scaffolding assumes the event model is not nested, I had to reroute all of the default redirects to include the current :business_id. This seems to be working.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :set_event, only: %i[show edit update destroy]
+  before_action :set_business, only: %i[index new show edit update destroy create]
 
   def index
     start_date = params.fetch(:start_date, Date.today).to_date
@@ -21,7 +22,7 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
+        format.html { redirect_to business_event_url(@business, @event), notice: "Event was successfully created." }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -33,7 +34,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to event_url(@event), notice: "Event was successfully updated." }
+        format.html { redirect_to business_event_url(@business, @event), notice: "Event was successfully updated." }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -46,7 +47,7 @@ class EventsController < ApplicationController
     @event.destroy!
 
     respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.html { redirect_to business_events_url(@business, @event), notice: "Event was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -55,6 +56,10 @@ class EventsController < ApplicationController
 
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def set_business
+    @business = Business.find(params[:business_id])
   end
 
   def event_params
