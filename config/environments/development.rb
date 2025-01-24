53f61db83e6e7d500f4795c6600fb8c46e2e70f2
Jaxon Avena <jaxonavena@gmail.com>
2025-01-24 11:29:20 -0600

    Switch to Sprockets from Webpacker
    Fixes: #64
    
    This PR is by no means complete, but I've successfully trasnferred Gymothy from webpacker to sprockets in order to be able to try using .scss files (which simple_calendar gem requires). Now the calendar looks great! Also I set some config values to make sure the server isn't caching anything in development so I can dynamically udpate the CSS with a refresh, instead of restarting the server entirely.
    
    Also, I've laid out some folders for future use under app/assets/stylesheets to maintain organization. I'm going to abandon the Rails template and stick with custom CSS.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,12 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Sprockets
+  config.assets.compile = true
+  config.assets.digest = true
+  config.assets.debug = true
+  config.assets.cache_store = :null_store
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
