bceeb84b3a47f24ff49ff64a995c256a69e31249
Jaxon Avena <jaxonavena@gmail.com>
2025-01-27 17:32:31 -0600

    Fill in Member spec scaffold
    I added a few specs to member_spec.rb related to memberships, visits, and events. I will have to come back to this and write more specs, because I'm about to be late for BJJ.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -16,9 +16,11 @@ require 'rails_helper'
 
 RSpec.describe Member, type: :model do
   let(:a_member) { build(:member) }
+  let(:a_business) { build(:business) }
 
   before do
     a_member.save!
+    a_business.save!
   end
 
   describe "validity" do
@@ -50,20 +52,55 @@ RSpec.describe Member, type: :model do
   end
 
   describe "visiting sites" do
+    let(:a_site) { build(:site, business: a_business) }
+
     it "can visit a site" do
+      expect(a_site.members.include?(a_member)).to eq(false)
+      a_member.visit(a_site)
+      expect(a_site.members.include?(a_member)).to eq(true)
     end
 
-    it "tracks the number of visits made to the site" do
+    xit "tracks the number of visits made to the site" do
+      a_member.visit(a_site)
+      visit = Visit.find_by(member_id: a_member, site_id: a_site)
+      expect(visit.count).to eq(1)
+      a_member.visit(a_site)
+      expect(visit.count).to eq(2)
     end
   end
 
   describe "memberships" do
-    it "can create a membership at a business" do
+    let(:a_membership) { build(:membership, member: a_member, business: a_business) }
+
+    before do
+      a_membership.save!
+    end
+
+    it "can have a membership at a business" do
+      expect(a_membership).to be_valid
+      expect(a_business.members.include?(a_member)).to eq(true)
     end
   end
 
   describe "events" do
-    it "can be interested in an event" do
+    let(:an_event) { build(:event, business: a_business) }
+
+    before do
+      an_event.save!
+    end
+
+    context "interests" do
+      let(:an_interest) { build(:interest, member: a_member, event: an_event) }
+
+      before do
+        an_interest.save!
+      end
+
+      it "can be interested in an event" do
+        expect(an_interest).to be_valid
+        expect(an_event.interested_members.include?(a_member)).to eq(true)
+        expect(a_member.interested_events.include?(an_event)).to eq(true)
+      end
     end
   end
 end
