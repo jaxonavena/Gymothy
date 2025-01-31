1f39a959812d0e1c1fe452f00d051e5116947226
Jaxon Avena <jaxonavena@gmail.com>
2025-01-31 10:10:22 -0600

    grid CSS, unique user email, user view css, add footer, make main nav links useful
    I did a lot in this commit, and probably should have been commiting stuff more individually, but I just jump around so much.
    
    - started working on grid css for better page layout displays, the first one will be in the membership#show page
    - I realized users could create new user objects with emails that have already been used, so I added a unique index on user email
    - I also realized the user/session views were still barren, so I at least added a nav bar to them
    - Added a persistent footer that follows you around the whole site

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_30_193928) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_31_065621) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -108,6 +108,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_30_193928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "visits", force: :cascade do |t|
