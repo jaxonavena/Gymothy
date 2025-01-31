3d38c8dbb423261db95dd2b9dc423eaba1c7fc35
Jaxon Avena <jaxonavena@gmail.com>
2025-01-30 23:18:43 -0600

    Add the ability for resolve_attr method to handle procs
    I finally got the subscription column to render correctly on sites#show in reference to each visitor. Maybe I was over thinking it, but it was actually pretty difficult. I finally got it to work by allowing the attrs list you pass to the partial to handle an attr that's a lambda. I forget if I mentioned in a previous commit that I made a SafeSend module to wrap public_send in some whitelist/blacklist stuff for allowed methods, but I did that. I tried to recreate the same idea for the lambdas by defining a SafeLambda class in safe_send.rb, but I haven't quite gotten it to work yet. Something about the args not being passed correctly, but I think it will be cool once it works, and it will be much more secure because randoms can't pass a lambda to run which is obviously way worse thanwhen I was only allowing strings that can run methods on the base object. Oops. Is all this technically metaprogramming? *he asks into the void*

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,29 @@ module ApplicationHelper
   include SafeSend
 
   def resolve_attr(object, attr)
-    # attr = string form dot notation to access on the object
-    # Ex.
-    #     object = some_membership_object
-    #     attr = "business.name"
-    #       -> some_membership_object.business.name
-    #
-    attr.to_s.split('.').inject(object) do |accum, method|
-      SafeSend.safe_send(accum, method)
-    end&.to_s&.upcase
+    if attr.is_a?(Proc)
+      # attr = lambda
+      # Ex.
+      #   object = some_visit_object
+      #   attr = lambda {|v| v.member.memberships.where(business_id: @site.business.id).first.status.upcase }
+
+      # TODO: Figure this out for security
+      # safe_lambda = SafeLambda.new(attr)
+      # safe_lambda.call(object)
+
+      attr.call(object)
+    elsif attr.is_a?(String)
+      # attr = string form dot notation to access on the object
+      # Ex.
+      #     object = some_membership_object
+      #     attr = "business.name"
+      #       -> some_membership_object.business.name
+
+      attr.to_s.split('.').inject(object) do |accum, method|
+        SafeSend.safe_send(accum, method)
+      end&.to_s&.upcase
+    else
+      "N/A"
+    end
   end
 end
--- app/models/concerns/safe_send.rb
+++ app/models/concerns/safe_send.rb
@@ -8,17 +8,37 @@ module SafeSend
     subscription
     status
     member
+    memberships
     count
   ].freeze
 
   BLOCKED_METHODS = %w[
     destroy
+    delete
+    drop
   ].freeze
 
   def safe_send(object, method)
-    return "#{method}" \
-      unless SAFE_METHODS.include?(method) && BLOCKED_METHODS.exclude?(method)
+    return method.to_s unless SAFE_METHODS.include?(method) && BLOCKED_METHODS.exclude?(method)
 
     object.public_send(method)
+  end
+  class SafeLambda
+    def initialize(&block)
+      @block = block
+    end
+
+    def call(*args)
+      if args.any? { |arg| BLACKLISTED_METHODS.map(&:to_i).include?(arg) }
+        raise ArgumentError, "Forbidden method call detected: #{args & BLACKLISTED_METHODS}"
+      end
+
+      # ensure correct arg count
+      if @block.arity != -1 && @block.arity != args.size
+        raise ArgumentError, "wrong number of arguments (given #{args.size}, expected #{@block.arity})"
+      end
+
+      @block.call(*args)
+    end
   end
 end
